201c670f5fa021af7efacd384880ef5ebfddf33c
MyUsername <emanuele.marini37@gmail.com>
2021-05-28 16:41:06 +0200

    prova inserimenti

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var numKm = prompt ("Quanti chilometri devi percorrere?");
+document.getElementById("numKm").innerHTML = numKm
+numKm=parseInt(numKm);
+var ageUser = prompt ("Quanti anni hai ?");
+document.getElementById("ageUser").innerHTML = ageUser
+ageUser=parseInt(ageUser);
+
+var price = (numKm*0,21);
+price=parseInt(price);
+document.getElementById("price").innerHTML = price
+
+
