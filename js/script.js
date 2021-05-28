6e335320c77ea84dc8b128507f8e8659f747e75f
MyUsername <emanuele.marini37@gmail.com>
2021-05-28 17:44:10 +0200

    prove sconto

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
 var numKm = prompt ("Quanti chilometri devi percorrere?");
 document.getElementById("numKm").innerHTML = numKm
 numKm=parseInt(numKm);
+
 var ageUser = prompt ("Quanti anni hai ?");
 document.getElementById("ageUser").innerHTML = ageUser
 ageUser=parseInt(ageUser);
 
-var price = (numKm*0,21);
+var price =(0,21 * parseInt(numKm)) ;
 price=parseInt(price);
 document.getElementById("price").innerHTML = price
+
+/*
+var discount;
+
+if (ageUser < 18){
+      discount === price / 100 * 20
+
+} else if (ageUser > 65) {
+      discount === price / 100 * 40
+} else {
+      discount = 0
+}
+
+var 
+
+*/
 
 
