48253c913ee649a73c11f08ba3b494b2c6d6c63d
MyUsername <emanuele.marini37@gmail.com>
2021-05-30 11:30:03 +0200

    riformulazione

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 var numKm = prompt ("Quanti chilometri devi percorrere?");
-document.getElementById("numKm").innerHTML = numKm
+document.getElementById("numKm").innerHTML = numKm;
 numKm=parseInt(numKm);
 
 var ageUser = prompt ("Quanti anni hai ?");
-document.getElementById("ageUser").innerHTML = ageUser
+document.getElementById("ageUser").innerHTML = ageUser;
 ageUser=parseInt(ageUser);
 
-var price =(0,21 * parseInt(numKm)) ;
-price=parseInt(price);
-document.getElementById("price").innerHTML = price
+var price = 0.21;
 
-/*
-var discount;
+var priceT = price * numKm ;
+document.getElementById("priceT").innerHTML = priceT;
+
+var priceDis;
+
+//calcolare sconto nell espressione e assegnare una sola var per entrambi gli sconti
+
+
 
 if (ageUser < 18){
-      discount === price / 100 * 20
+     var priceDis = priceT * 0.80; //sconto 20
 
 } else if (ageUser > 65) {
-      discount === price / 100 * 40
+     var priceDis = priceT * 0.60;//sconto 40
 } else {
-      discount = 0
+      var priceDis = priceT;
 }
 
-var 
 
-*/
+
+
 
 
